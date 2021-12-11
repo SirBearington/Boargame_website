2f5124f4221600f51c2bf636b860ae7243cba992
saa1saar <salli.saarinen@edu.turkuamk.fi>
2021-12-11 16:43:26 +0200

    created base.html, did changes to views.py and index.html

--- boardgame_app/urls.py
+++ boardgame_app/urls.py
@@ -1,1 +1,13 @@
-"""Defines URL patterns for boardgame_app."""from django.urls import pathfrom . import viewsapp_name = 'boardgame_app'urlpatterns = [    # Home page    path('', views.index, name='index'),]
+"""Defines URL patterns for boardgame_app."""
+from django.urls import path
+
+from . import views
+
+app_name = 'boardgame_app'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+    #Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+    
+]
--- boardgame_app/views.py
+++ boardgame_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
     """The home page for Boardgame website."""
     return render(request, 'boardgame_app/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'boardgame_app/topics.html', context)
+
 
 # Create your views here.
