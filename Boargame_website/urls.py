d678342814c7e7b9e1554a991ecadcd4cdd6257b
SirBearington <karl.bjorn@edu.turkuamk.fi>
2021-12-11 16:20:26 +0200

    .

--- Boargame_website/urls.py
+++ Boargame_website/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('Boargame_website.urls')),
-
+    path('admin/', admin.site.urls),
+    path('', include('boardgame_app.urls')),
 ]
--- boardgame_app/views.py
+++ boardgame_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+def index(request):
+    """The home page for Boardgame website."""
+    return render(request, 'boardgame_app/index.html')
+
 # Create your views here.
