ff237171e3e64e13c3220a92a1ea6052bef6f877
SirBearington <karl.bjorn@edu.turkuamk.fi>
2021-12-12 13:34:19 +0200

    upto page 534

--- boardgame_app/views.py
+++ boardgame_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 def index(request):
@@ -55,5 +55,23 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {'topic': topic, 'form': form}
     return render(request, 'boardgame_app/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('boardgame_app:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'boardgame_app/edit_entry.html', context)
 
 # Create your views here.
