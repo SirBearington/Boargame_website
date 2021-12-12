0c361a57a7346ec02674e03f58ee15a04ddae7d0
saa1saar <salli.saarinen@edu.turkuamk.fi>
2021-12-12 11:34:22 +0200

    added specific 'topic'/boardgame page in urls.py, new function views.py, created topic.html and linked it to topics.html

--- boardgame_app/urls.py
+++ boardgame_app/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     # Home page
     path('', views.index, name='index'),
     #Page that shows all topics.
-    path('topics/', views.topics, name='topics'),
-    
+    path('topics/', views.topics, name='topics'), # the 'word/' part tells Django to search for URLs that have the word,after base URL
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/',views.topic, name='topic'), # /<int:word_id>/ matches an integer into the // and store the value in word_id
+    # when a views funtction is called, the word_id is stored there as an argument
+
 ]
--- boardgame_app/views.py
+++ boardgame_app/views.py
@@ -12,5 +12,11 @@ def topics(request):
     context = {'topics': topics}
     return render(request, 'boardgame_app/topics.html', context)
 
+def topic(request,topic_id): # Here we have the topic_id from urls.py, it helps getting the right topic
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id) # get() retireves the topic we have added
+    entries = topic.entry_set.order_by('-date_added') #the minus in the 'date-added' sorts the list of topics in reverse order
+    context = {'topic': topic, 'entries': entries} #topic and entry get stored inside the context dictionary
+    return render(request, 'boardgame_app/topic.html', context) #the context dictionary gets send to the topic.html
 
 # Create your views here.
